e98b6f1d7074cad251382916728ab3c0f56cb2f4
Davis Vann Bennett <davis.v.bennett@gmail.com>
2021-12-25 00:46:18 -0500

    runnable day 1

--- 2021/d1/target/debug/d1.d
+++
@@ -1,1 +0,0 @@
-/Users/bennettd/dev/aoc/2021/d1/target/debug/d1: /Users/bennettd/dev/aoc/2021/d1/src/main.rs
--- 2021/d1/target/debug/deps/d1-e077ade014998ab1.d
+++
@@ -1,5 +0,0 @@
-/Users/bennettd/dev/aoc/2021/d1/target/debug/deps/d1-e077ade014998ab1: src/main.rs
-
-/Users/bennettd/dev/aoc/2021/d1/target/debug/deps/d1-e077ade014998ab1.d: src/main.rs
-
-src/main.rs:
